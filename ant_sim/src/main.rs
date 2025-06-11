ca346ccd98c6b52eb69c3149a649f743d018bd0d
Xander White <thexawman@gmail.com>
2025-06-11 03:27:51 -0400

    Update Tile pheramones, refractor and update And flags, create World structs and refractor world generation

--- ant_sim/src/main.rs
+++ ant_sim/src/main.rs
@@ -1,7 +1,10 @@
-const SIMULATION_HEIGHT: usize = 16;
-const SIMULATION_WIDTH: usize = 16;
+const SIMULATION_HEIGHT: usize = 32;
+const SIMULATION_WIDTH: usize = 128;
 const GROUND_HEIGHT : usize = 10;
-// const STARTING_ANT_COUNT: u8 = 5;
+
+const FOOD_SPAWNING_CHANCE_PERCENTAGE: u8 = 1;
+const STARTING_ANT_COUNT: u8 = 5;
+
 
 use crossterm::{
     cursor::{MoveTo},
@@ -9,7 +12,7 @@ use crossterm::{
     terminal::{Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen},
     execute,
 };
-// use rand::Rng;
+use rand::{Rng};
 use std::{io::{stdout, Write, Result}, time::Duration};
 // use std::thread;
 
@@ -19,13 +22,14 @@ pub struct Tile(u8);
 
 impl Tile {
     // Bit masks
-    // Has one free bit (bit 8)
-    const HAS_OBJECT_MASK: u8 = 0b0110_0000;
-    const PHEROMONE_LEVEL_MASK: u8 = 0b0001_1111;
+    const HAS_OBJECT_MASK: u8           = 0b1110_0000;
+    const FOOD_PHEROMONE_LEVEL_MASK: u8 = 0b0001_1000;
+    const HOME_PHEROMONE_LEVEL_MASK: u8 = 0b0000_0111;
 
-    const HAS_ANT: u8 = 1;
-    const HAS_FOOD: u8 = 2;
+    const HAS_ANT: u8      = 1;
+    const HAS_FOOD: u8     = 2;
     const HAS_OBSTACLE: u8 = 3;
+    // Has up to 7 for flags
 
     pub fn new(rng: &mut impl rand::Rng, food_chance_percentage: u8) -> Self {
         let mut tile = Tile(0);
@@ -47,7 +51,10 @@ impl Tile {
         (self.0 & Self::HAS_OBJECT_MASK) >> 5 == Self::HAS_OBSTACLE
     }
     pub fn pheromone(&self) -> u8 {
-        self.0 & Self::PHEROMONE_LEVEL_MASK
+        self.0 & Self::FOOD_PHEROMONE_LEVEL_MASK >> 3
+    }
+    pub fn home_pheromone(&self) -> u8 {
+        self.0 & Self::HOME_PHEROMONE_LEVEL_MASK
     }
 
     // Setters
@@ -73,24 +80,46 @@ impl Tile {
         }
     }
     pub fn set_pheromone(&mut self, level: u8) {
-        self.0 = (self.0 & !Self::PHEROMONE_LEVEL_MASK) | (level.min(31) & Self::PHEROMONE_LEVEL_MASK);
+        self.0 = (self.0 & !Self::FOOD_PHEROMONE_LEVEL_MASK) | ((level.min(3) & Self::FOOD_PHEROMONE_LEVEL_MASK) << 3);
+    }
+    pub fn set_home_pheromone(&mut self, level: u8) {
+        self.0 = (self.0 & !Self::HOME_PHEROMONE_LEVEL_MASK) | (level.min(7) & Self::HOME_PHEROMONE_LEVEL_MASK);
     }
 }
 
 
+enum Direction {
+    Up,
+    UpRight,
+    Right,
+    DownRight,
+    Down,
+    DownLeft,
+    Left,
+    UpLeft,
+}
+
+#[derive(Debug, Clone)]
 pub struct Ant(u16);
 
 impl Ant {
     //Bit masks
-    const MAX_HEALTH_MASK: u16 = 0b1100_0000_0000_0000;
+    const MAX_HEALTH_MASK: u16     = 0b1100_0000_0000_0000;
     const CURRENT_HEALTH_MASK: u16 = 0b0011_0000_0000_0000;
-    const STRENGTH_MASK: u16 = 0b0000_1100_0000_0000;
-    const DIRECTION_MASK: u16 = 0b0000_0000_1100_0000;
-    const SEES_OBSTACLE: u16 = 0b0000_0000_0010_0000;
-    const SEES_FOOD: u16 = 0b0000_0000_0001_0000;
-    const IS_LOST: u16 = 0b0000_0000_0000_0100;
-    const IS_DEAD: u16 = 0b0000_0000_0000_0010;
-    const IS_CARRYING_FOOD: u16 = 0b0000_0000_0000_0001;
+    const STRENGTH_MASK: u16       = 0b0000_1100_0000_0000;
+    const DIRECTION_MASK: u16      = 0b0000_0011_1000_0000;
+    const ANT_ROLE_MASK: u16       = 0b0000_0000_0110_0000;
+    const CARRYING_MASK: u16       = 0b0000_0000_0001_1000;
+    const UNIQUE_FLAGS_MASK: u16   = 0b0000_0000_0000_0111;
+
+    // Ant roles, 4 available (bits 6 and 7)
+    const WORKER: u16  = 0;
+    const SOLDIER: u16 = 1;
+    const SCOUT: u16   = 2;
+    const QUEEN: u16   = 3;
+    // Carrying flags, 3 available (bits 3 and 4)
+    const IS_CARRYING_FOOD: u16 = 1;
+    // Unique flags, 8 available (bits 0 to 2)
 
     pub fn new() -> Self {
         Ant(0)
@@ -107,22 +136,13 @@ impl Ant {
         (self.0 & Self::STRENGTH_MASK) >> 10
     }
     pub fn direction(&self) -> u16 {
-        (self.0 & Self::DIRECTION_MASK) >> 6
+        (self.0 & Self::DIRECTION_MASK) >> 7
     }
-    pub fn sees_obstacle(&self) -> bool {
-        self.0 & Self::SEES_OBSTACLE != 0
+    pub fn carrying_food(&self) -> bool {
+        (self.0 & Self::CARRYING_MASK) >> 3 == Self::IS_CARRYING_FOOD
     }
-    pub fn sees_food(&self) -> bool {
-        self.0 & Self::SEES_FOOD != 0
-    }
-    pub fn is_lost(&self) -> bool {
-        self.0 & Self::IS_LOST != 0
-    }
-    pub fn is_dead(&self) -> bool {
-        self.0 & Self::IS_DEAD != 0
-    }
-    pub fn is_carrying_food(&self) -> bool {
-        self.0 & Self::IS_CARRYING_FOOD != 0
+    pub fn this_type(&self) -> u16 {
+        (self.0 & Self::ANT_ROLE_MASK) >> 3
     }
 
     // Setters
@@ -138,39 +158,58 @@ impl Ant {
     pub fn set_direction(&mut self, direction: u16) {
         self.0 = (self.0 & !Self::DIRECTION_MASK) | ((direction & 0b11) << 6);
     }
-    pub fn set_sees_obstacle(&mut self, value: bool) {
-        if value {
-            self.0 |= Self::SEES_OBSTACLE;
-        } else {
-            self.0 &= !Self::SEES_OBSTACLE;
-        }
-    }
-    pub fn set_sees_food(&mut self, value: bool) {
-        if value {
-            self.0 |= Self::SEES_FOOD;
-        } else {
-            self.0 &= !Self::SEES_FOOD;
-        }
-    }
-    pub fn set_is_lost(&mut self, value: bool) {
-        if value {
-            self.0 |= Self::IS_LOST;
-        } else {
-            self.0 &= !Self::IS_LOST;
-        }
-    }
-    pub fn set_dead(&mut self, value: bool) {
-        if value {
-            self.0 |= Self::IS_DEAD;
-        } else {
-            self.0 &= !Self::IS_DEAD;
-        }
-    }
     pub fn set_carrying_food(&mut self, value: bool) {
         if value {
-            self.0 |= Self::IS_CARRYING_FOOD;
-        } else {
+            self.0 = (self.0 & !Self::CARRYING_MASK) | Self::IS_CARRYING_FOOD << 3;
+        } else if Self::carrying_food(self) {
             self.0 &= !Self::IS_CARRYING_FOOD;
+        }
+    }
+}
+
+
+pub struct AntUnit {
+    pub ant: Ant,
+    pub x: usize,
+    pub y: usize,
+}
+
+
+pub struct World {
+    pub grid: [Tile; SIMULATION_HEIGHT * SIMULATION_WIDTH],
+    pub ants: Vec<AntUnit>,
+}
+
+impl World {
+    pub fn new(rng: &mut impl rand::Rng) -> Self {
+        let grid = std::array::from_fn(|_| Tile::new(rng, FOOD_SPAWNING_CHANCE_PERCENTAGE));
+        let ants = Vec::with_capacity(STARTING_ANT_COUNT as usize);
+
+        World { grid, ants }
+    }
+
+    pub fn idx(x: usize, y: usize) -> usize {
+        y * SIMULATION_WIDTH + x
+    }
+
+    pub fn has_food(&self, x: usize, y: usize) -> bool {
+        let index: usize = Self::idx(x, y);
+        self.grid[index].has_food()
+    }
+
+    pub fn add_ant(&mut self, x: usize, y: usize) {
+        let ant = Ant::new();
+        self.ants.push(AntUnit { ant, x, y });
+        let index: usize = Self::idx(x, y);
+        self.grid[index].set_ant(true);
+    }
+
+    pub fn get_tile_mut(&mut self, x: usize, y: usize) -> Option<&mut Tile> {
+        if x < SIMULATION_WIDTH && y < SIMULATION_HEIGHT {
+            let index: usize = Self::idx(x, y);
+            Some(&mut self.grid[index])
+        } else {
+            None
         }
     }
 }
@@ -199,9 +238,15 @@ fn display_tile(tile: &Tile) -> char {
 }
 
 
-fn idx(x: usize, y: usize) -> usize {
-    y * SIMULATION_WIDTH + x
-}
+// fn ant_role_name(role: u8) -> &'static str {
+//     match role {
+//         0 => "Worker",
+//         1 => "Soldier",
+//         2 => "Scout",
+//         3 => "Queen",
+//         _ => "Unknown",
+//     }
+// }
 
 
 fn run_simulation() -> Result<()> {
@@ -209,32 +254,28 @@ fn run_simulation() -> Result<()> {
     let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
 
     // Initialize grid with random food
-    let mut grid: [Tile; SIMULATION_HEIGHT * SIMULATION_WIDTH] = std::array::from_fn(|_| Tile::new(&mut rng, 10));
+    let mut world = World::new(&mut rng);
+    
     // Set ant
-    let index: usize = idx(SIMULATION_WIDTH / 2 - 1, SIMULATION_HEIGHT - GROUND_HEIGHT - 1);
-    grid[index].set_ant(true);
+    for _ in 0..STARTING_ANT_COUNT {
+        let x = rng.gen_range(0..SIMULATION_WIDTH);
+        let y = rng.gen_range(0..SIMULATION_HEIGHT - GROUND_HEIGHT);
+        world.add_ant(x, y);
+    }
     // Set ground
     for y in SIMULATION_HEIGHT - GROUND_HEIGHT..SIMULATION_HEIGHT {
         for x in 0..SIMULATION_WIDTH {
-            let index: usize = idx(x, y);
-            grid[index].set_obstacle(true);
+            if let Some(tile) = world.get_tile_mut(x, y) {
+                tile.set_obstacle(true);
+            }
         }
     }
-
-    // let test_tile = &mut grid[idx(1, 1)];
-    // test_tile.set_pheromone(5);
-    // test_tile.set_ant(true);
-    // println!("Pheromone level: {}", test_tile.pheromone().to_string());
-    // println!("Has ant: {}", test_tile.has_ant());
-    // println!("Has food: {}", test_tile.has_food());
-    // test_tile.set_food(true);
-    // println!("Has food: {}", test_tile.has_food());
 
     loop {
         clear_screen();
         println!("Use WASD to move. Press 'q' to quit.\n");
 
-        for row in grid.chunks(SIMULATION_WIDTH) {
+        for row in world.grid.chunks(SIMULATION_WIDTH) {
             let row_display: String = row.iter().map(display_tile).collect();
             println!("{}", row_display);
         }
@@ -269,6 +310,9 @@ fn main() {
 
     execute!(stdout, LeaveAlternateScreen).unwrap();
     crossterm::terminal::disable_raw_mode().unwrap();
+
+    clear_screen();
+    println!("Goodbye.");
 
     if let Err(err) = result {
         eprintln!("Simulation error: {}", err);
